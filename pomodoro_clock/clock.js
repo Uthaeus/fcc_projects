a4a45c506227e606e6a397b11de98a5c0aa87b16
Uthaeus <romanlavery@gmail.com>
2018-09-06 11:22:00 -0600

    Implemented incrementers and decrementers, working on countdown functionality

--- pomodoro_clock/clock.js
+++ pomodoro_clock/clock.js
@@ -2,32 +2,57 @@ let breakMin = document.getElementById('break-length');
 let sessionMin = document.getElementById('session-length');
 let timerMin = document.getElementById('minutes');
 let timerSec = document.getElementById('seconds');
+let runClock = false;
 
 
 function breakDecrement() {
-  console.log(breakMin.value);
-  let newTime = breakMin.data - 1;
-  breakMin.innerHTML = newTime;
+  if (breakMin.innerHTML > 0) {
+    breakMin.innerHTML--;
+  }
 }
 
 function breakIncrement() {
-
+  if (breakMin.innerHTML < 60) {
+    breakMin.innerHTML++;
+  }
 }
 
 function sessionDecrement() {
-
+  if (sessionMin.innerHTML > 0) {
+    sessionMin.innerHTML--;
+    timerMin.innerHTML = sessionMin.innerHTML;
+    timerSec.innerHTML = 00;
+  }
 }
 
 function sessionIncrement() {
-
+  if (sessionMin.innerHTML < 60) {
+    sessionMin.innerHTML++;
+    timerMin.innerHTML = sessionMin.innerHTML;
+    timerSec.innerHTML = 00;
+  }
 }
 
-function setBreak() {
+function startStop() {
+  runClock = !runClock;
 
-}
-
-function setSession() {
-
+  if (runClock) {
+    if (timerMin.innerHTML > 0 && timerSec.innerHTML > 0) {
+      let mins = timerMin.innerHTML, secs = timerSec.innerHTML;
+      setInterval(function() {
+        timerMin.innerHTML = mins;
+        timerSecs.innerHTML = secs;
+        secs--;
+        if (secs == 00) {
+          mins--;
+          secs = 60;
+          if (mins == 0 && secs == 0) {
+            countOver();
+          }
+        }
+      }, 1000);
+    }
+  }
 }
 
 function resetClock() {
@@ -35,4 +60,5 @@ function resetClock() {
   sessionMin.innerHTML = 25;
   timerMin.innerHTML = 25;
   timerSec.innerHTML = 00;
+  runClock = false;
 }
